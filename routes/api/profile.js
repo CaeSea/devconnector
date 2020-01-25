9606d1fc6177fcc3a12f9c64f97b9d94ecdb122b
Caeron Seabourne <caeking93@gmail.com>
2020-01-25 18:15:19 +0000

    Created the delete user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -135,4 +135,22 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route    DELETE api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo: Remove users posts
+
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    await Profile.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'Your account has been deleted.' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 module.exports = router;
