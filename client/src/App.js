8122a80f95c10f7a9e3d6f6c1f0012c75a1e75dd
Caeron Seabourne <caeking93@gmail.com>
2020-01-30 19:11:21 +0000

    CreateProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 
@@ -39,6 +40,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
