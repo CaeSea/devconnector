87e38b5b658fe44a3280db196515be7c32325339
Caeron Seabourne <caeking93@gmail.com>
2020-02-01 20:24:39 +0000

    ProfileTop and ProfileAbout components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({
   getProfileById,
@@ -15,7 +17,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(id);
-  }, [getProfileById]);
+  }, [getProfileById, id]);
 
   return (
     <Fragment>
@@ -33,6 +35,10 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
+          <div class="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
         </Fragment>
       )}
     </Fragment>
